7a2e2c92edf40a9a2398867ddec56f0bac503d79
ikishanm27 <ikishanm27@gmail.com>
2026-01-15 12:19:43 +0530

    Update db.py
    db_url changed to database_url

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite:///./employees.db"
 
-engine=create_engine(db_url , connect_args={"check_same_thread": False})
+engine=create_engine(DATABASE_URL , connect_args={"check_same_thread": False})
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
 
